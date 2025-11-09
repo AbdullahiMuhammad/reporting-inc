63a450ed472895a14b55a84bb3ee5b254b787d13
muheey <54740134+AbdullahiMuhammad@users.noreply.github.com>
2025-11-09 21:52:24 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -6,5 +6,5 @@ import axios from "axios";
     }
 });
 
-export const proxy = "reporting-inc-server.vercel.app:5000/api";
+export const proxy = "reporting-inc-server.vercel.app/api";
 export default axiosInstance;
